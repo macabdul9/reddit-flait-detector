494b2133ba9d62634bbbee8d1ca765a70fa93297
macabdul9 <abdulwaheed1513@gmail.com>
2020-04-15 03:00:45 +0530

    add files via terminal

---
+++ notebooks/change_bar_width.py
@@ -0,0 +1,10 @@
+def change_width(ax, new_value) :
+    for patch in ax.patches :
+        current_width = patch.get_width()
+        diff = current_width - new_value
+
+        # we change the bar width
+        patch.set_width(new_value)
+
+        # we recenter the bar
+        patch.set_x(patch.get_x() + diff * .5)
