c9c59838c1fc777d9a827da49e790ea3a3703f2b
macabdul9 <abdulwaheed1513@gmail.com>
2020-04-21 17:30:14 +0530

    add files via terminal

--- notebooks/rfd/utility.py
+++ notebooks/rfd/utility.py
@@ -32,11 +32,11 @@ def autolabel(ax, dtype=int):
     """Attach a text label above each bar in *rects*, displaying its height."""
     for bar in ax.patches:
         if dtype==float:
-            height = str(bar.get_height())[:4]
+            text = str(bar.get_height())[:4]
         else:
-            height = str(int(bar.get_height()))
-        ax.annotate(height,
-                    xy=(bar.get_x() + bar.get_width() / 2, height),
+            text = str(int(bar.get_height()))
+        ax.annotate(text,
+                    xy=(bar.get_x() + bar.get_width() / 2, bar.get_height()),
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
                     ha='center', va='bottom')
